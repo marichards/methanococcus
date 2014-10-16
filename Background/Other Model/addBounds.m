d469bb35e2ef07e13d7051b3fccac168692b0335
marichards <marichards88@gmail.com>
2014-10-15 18:28:01 -0700

    Their Model Finishing

--- Background/Other Model/addBounds.m
+++ Background/Other Model/addBounds.m
@@ -1,22 +1,30 @@
-function model = addBounds(model)
+function [model,unchanged] = addBounds(model)
 
-%For all reactions that are reversible, make the bounds -1000 and 1000
-%For all irreversible reactions, make the bounds 0 and 1000
+%For all reactions, examine the lower and upper bounds, attach them
+%accordingly
 
-%Loop through the reactions
-for i = 1:length(model.rxns)
+%Load the bounds
+load('2014_10_13_bounds.mat');
+
+%Intersect the model indices with the bounds ones (the dictionary)
+[~,model_idx,dict_idx] = intersect(model.rxns,rxn_labs);
+
+%Loop through the indices and put things in
+for i = 1:length(model_idx)
     
-    %Make the UB 1000
-    model.ub(i) = 1000;
+    %Make the bounds match up
+    model.lb(model_idx(i)) = bounds(dict_idx(i),1);
+    model.ub(model_idx(i)) = bounds(dict_idx(i),2);
     
-    %Look at the model.rev value
-    %If it's 1, then make the lower bound -1000
-    %Otherwise, make it 0
-    if model.rev(i) == 1
-        %Then make it -1000
-        model.lb(i)=-1000;
-    else
-        %Make it 0
-        model.lb(i)=0;
+    %Change model.rev values too; right now all are reversible
+    if model.lb(model_idx(i)) == 0
+        
+        %Change rev to 0
+        model.rev(model_idx(i)) = 0;
     end
+        
+    
 end
+
+%Finally, return reactions that didn't show up
+unchanged = setdiff(model.rxns,rxn_labs);
