bc1576dda0574a766f25e550a7ff0d4019153958
marichards <marichards88@gmail.com>
2015-03-30 18:48:32 -0700

    Changed code to switch the model around

--- Matt_Matlab/Strain Design/makeMEOHReduceSulfate.m
+++ Matt_Matlab/Strain Design/makeMEOHReduceSulfate.m
@@ -1,31 +1,15 @@
-function [solution,model] = makeMEOHReduceSulfate(model)
-
-%Add in the reaction for converting methanol to methyl-coM
-model = addReaction(model,'Methanol_to_MCoM',...
-    'Methanol_c0 + CoM_c0 + H_c0 <=> Methyl_CoM_c0 + H2O_c0');
-
-%Add in the methanol uptake; this is now connected to the methanogenesis
-%pathway
-model = addReaction(model,'EX_methanol_c0',...
-    'Methanol_c0 <=> ');
+function solution = maxGrowthOnMethane(model)
 
 %Turn off the MFR
 %model = changeRxnBounds(model,'rxn11938_c0',0,'b');
 
-%Change the media to SUPPLY methane
-
 %Overall methanol reaction(s): 
 %H2O + SO4 + 5 CH4 = H2S + 5 CH3OH
+%CH4 + 4 NO3- -> CO2 + 4 NO2- + 2 H2O
 
 % From paper:
 %CH4 + SO4(-2) -> HCO3- + HS- + H2O
-%CH3 + 4 NO3- -> CO2 + 4 NO2- + 2 H2O
-
-%Add in sulfur supply
-model = addReaction(model,{'rxn05651_c0','sulfate transport in via proton symport c0'},...
-    'H_e0 + Sulfate_e0 <=> H_c0 + Sulfate_c0');
-model = addReaction(model,{'EX_cpd00048_e0','EX_Sulfate_e0'},...
-    'Sulfate_e0 <=> ');
+%CH4 + 4 NO3- -> CO2 + 4 NO2- + 2 H2O
 
 %Change bounds such that methane goes IN instead of OUT
 model = changeRxnBounds(model,'Ex_cpd01024_c0',-1000,'l');
@@ -49,6 +33,7 @@ solution = optimizeCbModel(model,[],'one');
 [~,h2o_idx] = intersect(model.rxns,'EX_cpd00001_e0');
 [~,nh3_idx] = intersect(model.rxns,'EX_cpd00013_e0');
 [~,po4_idx] = intersect(model.rxns,'EX_cpd00009_e0');
+[~,so4_idx] = intersect(model.rxns,'EX_cpd00048_e0');
 
 
 %Print the biomass flux
@@ -61,12 +46,15 @@ fprintf('H2O flux: %f\n',solution.x(h2o_idx))
 fprintf('CH4 flux: %f\n',solution.x(ch4_idx))
 fprintf('NH3 flux: %f\n',solution.x(nh3_idx))
 fprintf('PO4 flux: %f\n',solution.x(po4_idx))
+fprintf('SO4 flux: %f\n',solution.x(so4_idx))
 
 %Print the per-CO2 actual reaction
-fprintf('\nExpected Overall reaction:\nCH3OH + H2 --> H2O + CH4\n')
+fprintf('\nExpected Overall reaction:\nH2O + SO4 + 5 CH4 = H2S + 5 CH3OH\n')
 fprintf('\nModel overall reaction (per mole CH4)\n')
-fprintf('%0.2f CH3OH + %0.2f H2 --> %0.2f H2O + CH4\n\n',-solution.x(meoh_idx)/solution.x(ch4_idx),...
-    -solution.x(h2_idx)/solution.x(ch4_idx),solution.x(h2o_idx)/solution.x(ch4_idx))
+fprintf('%0.2f H2O + %0.2f SO4 + CH4 --> %0.2f H2S + CH3OH\n\n',-solution.x(h2o_idx)/solution.x(ch4_idx),...
+    -solution.x(so4_idx)/solution.x(ch4_idx),...
+    solution.x(h2s_idx)/solution.x(ch4_idx),...
+    solution.x(meoh_idx)/solution.x(ch4_idx))
 
 %Print the yield coefficient (grams biomass per mole CH4 produced)
 fprintf('Predicted Yield Coefficient: %0.3f gDCW/mol CH4\n\n',solution.f*1000/solution.x(ch4_idx))
