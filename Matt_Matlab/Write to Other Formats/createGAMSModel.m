0a5fa0602d3107b6b15821a7c6f35631f236a2c8
marichards <marichards88@gmail.com>
2015-04-20 17:18:50 -0700

    GAMS model code finished and used

--- Matt_Matlab/Write to Other Formats/createGAMSModel.m
+++ Matt_Matlab/Write to Other Formats/createGAMSModel.m
@@ -15,31 +15,34 @@ file_id = fopen(sprintf('%s.txt',model_filename),'w');
 fprintf(file_id,'%s for use in SimOptStrain\n\n',model_filename);
 
 % Print metabolites set
-fprintf(file_id,'Sets\ni metabolites in S (m)\n/');
+fprintf(file_id,'Sets\ni metabolites in S (m)\n/\n');
 
 for i=1:length(model.mets)
-    fprintf(file_id,'%s',model.mets{i});
-    if i<length(model.mets)
-        fprintf(file_id,',');
-    end
+    fprintf(file_id,'%s\n',model.mets{i});
 end
 
 % More formatting
-fprintf(file_id,'\n/\nj reactions in S (n)\n/');
+fprintf(file_id,'/\nj reactions in S (n)\n/\n');
 
 % Print reactions set
 for j=1:length(model.rxns)
-    fprintf(file_id,'%s',model.rxns{j});
-    if j<length(model.rxns)
-        fprintf(file_id,',');
-    end
+    fprintf(file_id,'%s\n',model.rxns{j});
 end
 
 % More formatting; No upper/lower limits yet
-fprintf(file_id,'\n/\nParameters\nVmax /1000/\n');
-fprintf(file_id,'UpperLimits(j) maximum value flux can take\n');
-fprintf(file_id,'LowerLimits(j) minimum value flux can take\n/\(\n');
-fprintf(file_id,'\n\)-1000/\nS(i,j) contains the S matrix\n/\n');
+fprintf(file_id,'/\nParameters\n\n');
+% Do upper and lower bounds how Vangelis specified
+% Print each reaction and its upper bound
+fprintf(file_id,'UpperLimits(j) maximum value flux can take\n/\n');
+for j=1:length(model.rxns)
+    fprintf(file_id,'%s\t%0.2f\n',model.rxns{j},model.ub(j));
+end
+% Print each reaction and its lower bound
+fprintf(file_id,'/;\n\nLowerLimits(j) minimum value flux can take\n/\n');
+for j=1:length(model.rxns)
+    fprintf(file_id,'%s\t%0.2f\n',model.rxns{j},model.lb(j));
+end
+fprintf(file_id,'/;\n\nS(i,j) contains the S matrix\n/\n');
 
 %Make non-sparse matrix A
 A = full(model.S);
