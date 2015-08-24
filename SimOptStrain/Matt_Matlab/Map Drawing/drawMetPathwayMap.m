028f9f0b984f812d8a0aba8d4706c4e7b864c31d
marichards <marichards88@gmail.com>
2015-08-24 13:29:13 -0700

    Removed Matt_Matlab from simoptstrain, add tom edits

--- SimOptStrain/Matt_Matlab/Map Drawing/drawMetPathwayMap.m
+++
@@ -1,5 +0,0 @@
-function [rxns,fluxes,rxnNames] = drawMetPathwayMap(model,met,solution)
-
-%Take in a model, a metabolite of interest, and flux solution, then draw a map with Paint4Net
-[rxns,fluxes,rxnNames] = traceFullMetPath(model,met,solution,{'biomass0'});
-draw_by_rxn(model,rxns,'true','struc',{''},{''},solution.x)
--- SimOptStrain/Matt_Matlab/Map Drawing/drawRxnsMap.m
+++
@@ -1,4 +0,0 @@
-function [rxns,fluxes,rxnNames] = drawRxnsMap(model,rxns,solution)
-
-%Take in a model, a reaction set of interest, and flux solution, then draw a map with Paint4Net
-draw_by_rxn(model,rxns,'true','struc',{''},{''},solution.x)
