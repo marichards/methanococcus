91c1ccd93608c946e7adf9dbcce6c324d40ea783
marichards <marichards88@gmail.com>
2015-11-20 11:55:20 -0800

    Made fdh reversible

--- Model/createLatestModel.m
+++ Model/createLatestModel.m
@@ -2286,6 +2286,12 @@ model.subSystems{idx} = 'Amino Acid Biosynthesis';
 model.subSystems{idx} = 'Amino Acid Biosynthesis';
 
 %%%%%%%%%%%%%
+%11/20/2015
+%%%%%%%%%%%%%
+% Make Fdh reversible
+model = changeRxnBounds(model,'Fdh',0,'l');
+
+%%%%%%%%%%%%%
 %7/14/2015
 %%%%%%%%%%%%%
 
