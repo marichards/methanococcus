d1397410893571616eaeff9c9b7f79fea711c971
marichards <marichards88@gmail.com>
2016-01-08 14:24:56 -0800

    Adjusted manuscripts

---
+++ Matt_Matlab/Strain Design/tryManualDesigns.m
@@ -0,0 +1,14 @@
+function tryManualDesigns
+
+% Test out the manual strain designs from Dr. John Leigh
+
+% Design#1: Add in a reduction pathway that uses H2 directly; need to clone
+% in a hydrogenase too
+
+% Design#2: Add in H2 -> Fd reaction plus a reduction pathway (e.g. nitrate)
+% then require that free energy be negative
+
+% Design#3: Use Fumarate --> Succinate conversion to reform disulfide; has
+% no way to account for fd though! [Rxn is 'Tfr' and goes correct way
+% already]
+
